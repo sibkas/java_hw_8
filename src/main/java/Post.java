b00ca5e44d8429c9554ca68924147df3506d5aa1
akovr <akovr@yahoo.com>
2025-07-25 10:36:43 +0700

    first commit

---
+++ src/main/java/Post.java
@@ -0,0 +1,9 @@
+public class Post {
+    public String name;
+    public String surname;
+    public String patronymic;
+    public String phone;
+    public String passport;
+    public boolean subscription;
+    public FormDate birthday;
+}
